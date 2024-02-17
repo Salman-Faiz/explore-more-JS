157bbca735c4e2929ece78ebe536c0838fb01ece
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-02-17 14:19:16 +0600

    assignment 1 correction

--- assignment01.js
+++ assignment01.js
@@ -1,8 +1,12 @@
 function cubeNumber(number){
+    if(typeof number !== 'number'){
+        console.log('please enter a number');
+     
+    }
     let result =1;
     result= number * number * number;
   console.log(result);  
     return result;
 }
 
-cubeNumber(3);
+cubeNumber(5);
