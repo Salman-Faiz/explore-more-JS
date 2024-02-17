7e68595f199b6e875b79589a2c469f57154f845a
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-02-17 15:37:58 +0600

    using match funcion to mataches the values of two strings

---
+++ assignment02.js
@@ -0,0 +1,19 @@
+function matchFinder(str1, str2) {
+    if (typeof str1 === 'string' && typeof str2 === 'string') {
+        
+// match function is used for matching the value of two sreings
+            if (str1.match(str2)) {
+                console.log('true');
+            }
+            else {
+                console.log('false');
+            }
+        }
+    else {
+        console.log('please enter a number');
+    }
+
+}
+const a = 'john hardy';
+const b = 'john';
+matchFinder(a, b);
